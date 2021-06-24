e50a125af71c5306c0188efaa9aaf36eaa703862
ChinemeremEze <ezeakudoluchinemerem@gmail.com>
2021-06-24 13:28:04 -0400

    Added a new reducer for thunks

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import { connect } from 'react-redux';
 import { removeTodo , completeTodo} from './action';
 import TodoListItem from './TodoListItem'; 
 import NewTodoForm from './NewTodoForm';
-import { displayAlert } from './thunks';
+import { loadTodos } from './thunks';
 import './TodoList.css'
 
-function TodoList({todos = [], onRemovePressed, onCompletePressed}){
-    return(
+function TodoList({todos = [], onRemovePressed, onCompletePressed, isLoading, startLoadingTodos}){
+
+    useEffect(() => {
+        startLoadingTodos();
+    }, [])
+
+    const loadingMessage = <div>loading todos....</div>;
+    const content = (
         <div className="list-wrapper">
             <NewTodoForm/>
             {todos.map(todo => 
@@ -19,13 +25,16 @@ function TodoList({todos = [], onRemovePressed, onCompletePressed}){
             }
         </div>
     );
-}
+    return isLoading? loadingMessage: content;
+};
 const mapStateToProps = state =>({
+    isLoading: state.isLoading,
     todos: state.todos,
 });
 
 const mapDispatchToProps = dispatch =>({
     onRemovePressed: text => dispatch(removeTodo(text)),
     onCompletePressed: text => dispatch(completeTodo(text)),
+    startLoadingTodos: () => dispatch(loadTodos())
 });
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
