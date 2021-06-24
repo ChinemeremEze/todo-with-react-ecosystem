574b44068139196324017d3a1f103b6d1b40d7ce
ChinemeremEze <ezeakudoluchinemerem@gmail.com>
2021-06-24 12:47:42 -0400

    Add multiple async thunks

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,3 +1,17 @@
+import { loadTodosSuccess, loadTodosFailure, loadTodosInProgress } from "./action";
+export const loadTodos = () => async(dispatch, getState) =>{
+    try{
+        dispatch(loadTodosInProgress())
+        const response = await fetch('http://localhost:8080/todos');
+        const todos = await response.json();
+
+        dispatch(loadTodosSuccess(todos));
+    }catch(e){
+        dispatch(loadTodosFailure());
+        dispatch(displayAlert(e));
+    }
+}
+
 export const displayAlert = text =>() =>{
-    alert(`You clicked on ${text}`);
+    alert(text);
 };
